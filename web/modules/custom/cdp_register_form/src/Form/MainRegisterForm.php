df06512e3512355ff421ae3c88412b6aed789a6e
ugas19 <ugnius_staniulis@msn.com>
2020-02-20 10:50:52 +0200

    Fix_6 registration module

--- web/modules/custom/cdp_register_form/src/Form/MainRegisterForm.php
+++ web/modules/custom/cdp_register_form/src/Form/MainRegisterForm.php
@@ -48,12 +48,14 @@ class MainRegisterForm extends RegisterForm {
    */
   public function makeName($name) {
     $newName = explode('@', $name);
-    return $newName[0];
+    return $newName[0] . '_' . explode('.',$newName[1])[0];
   }
   public function save(array $form, FormStateInterface $form_state) {
     $storage = $form_state->getStorage();
-    $role_id = $storage['role'];
-    $this->entity->set('roles', $role_id);
+    if($storage['role']){
+       $role_id = $storage['role'];
+       $this->entity->set('roles', $role_id);
+    }
     parent::save($form, $form_state);
   }
 }
